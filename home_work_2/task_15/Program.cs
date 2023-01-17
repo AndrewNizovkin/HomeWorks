a906e49c654ffbd95c29214900be91e68b2f00d6
andrew_nizovkin <nizovkinandrej@gmail.com>
2023-01-17 16:40:55 +0300

    Added home_work_2 tasks

---
+++ home_work_2/task_15/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите номер дня недели--> ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if (dayNumber > 0 & dayNumber < 8) 
+{
+    if (dayNumber > 5)
+    {
+        Console.Write("Это выходной день");
+    }
+    else
+    {
+        Console.Write("Это не выходной день");
+    }
+}
+else
+{
+    Console.Write("Это не номер дня недели");
+}
