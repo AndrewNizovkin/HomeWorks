cc1c2d50a494bfb85c62bf4e3ca676826a1ecada
andrew_nizovkin <nizovkinandrej@gmail.com>
2023-01-18 11:46:29 +0300

    Added work_on_bugs_13

---
+++ home_work_2/work_on_bugs_13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите натуральное число--> ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.Write("Третьей цифры нет");
+}
+else
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+
+    Console.Write(number % 10);
+}
