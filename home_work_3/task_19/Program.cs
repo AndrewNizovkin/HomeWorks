7acededb110e11f8ba2654dc34860d78ad7abdb2
andrew_nizovkin <nizovkinandrej@gmail.com>
2023-01-19 21:26:21 +0300

    Added home_work_3

---
+++ home_work_3/task_19/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// Gets revers number
+int reversNumber(int number)
+{
+    double revers = 0;
+    int count = 5;
+    
+    while (number > 0)
+    {
+        revers += (number % 10) * Math.Pow(10.0, (double)(count - 1));
+        number = number / 10;
+        count--;
+    }
+
+    return (int)revers;
+}
+
+Console.Write("Введите пятизначное натуральное число--> ");
+int naturalNumber = Convert.ToInt32(Console.ReadLine());
+
+if (reversNumber(naturalNumber) == naturalNumber)
+{
+    Console.Write("Это палиндром");
+}
+else
+{
+    Console.Write("Это не палиндром");
+}
+
---
+++ home_work_3/task_21/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// Gets point coordinates
+double[] getCoordinates(string name)
+{
+    double[] newPoint = new double[3];
+
+    Console.Write($"Введите X точки {name} --> ");
+    newPoint[0] = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write($"Введите Y точки {name} --> ");
+    newPoint[1] = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write($"Введите Z точки {name} --> ");
+    newPoint[2] = Convert.ToDouble(Console.ReadLine());
+
+    return newPoint;
+}
+
+double[] A = getCoordinates("A");
+double[] B = getCoordinates("B");
+
+double distanse = Math.Sqrt(Math.Pow((A[0] - B[0]), 2) + 
+Math.Pow((A[1] - B[1]), 2) + 
+Math.Pow((A[2] - B[2]), 2));
+
+Console.WriteLine(Math.Round(distanse, 2));
