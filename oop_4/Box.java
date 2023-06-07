9d6221df05ea58c1dd7786d66aa88e0cb0266d10
andrew_nizovkin <nizovkinandrej@gmail.com>
2023-06-07 18:14:57 +0300

    Home work oop_4 ch2

--- oop_4/Box.java
+++ oop_4/Box.java
@@ -47,7 +47,7 @@ public class Box <T extends Fruit>{
      * @param box instance Box
      * @return boolean
      */
-    public boolean compare(Box<? extends Fruit> box) {
+    public boolean compare(Box<?> box) {
         return this.getWeight() - box.getWeight() == 0;
     }
 
