eb65dad78b4453b39ee74a9055627429532f6c4a
andrew_nizovkin <nizovkinandrej@gmail.com>
2023-09-27 10:01:28 +0300

    fix: Home work exception

--- exceptions_3/Writer.java
+++ exceptions_3/Writer.java
@@ -8,14 +8,13 @@ public class Writer implements FileWriteble{
     /**
      * Writes line to file
      * @param filePath file name
-     * @param line     String line
+     * @param line String line
      */
     @Override
     public void writeToFile(String filePath, String line) {
         File file = new File(filePath);
             try(FileWriter fileWriter = new FileWriter(file,true)) {
-                fileWriter.write(line + "\n" +
-                        "");
+                fileWriter.write(line + "\n");
             } catch (IOException e) {
                 e.printStackTrace();
             }
