c8a55868cfc834ba0cb8f964d73d97ba4705027f
andrew_nizovkin <nizovkinandrej@gmail.com>
2023-11-28 15:11:52 +0300

    Fix: update Validator.java

--- java_core_4/lesson4/presenter/Validator.java
+++ java_core_4/lesson4/presenter/Validator.java
@@ -7,15 +7,16 @@ public interface Validator {
     /**
      * Checks the validity of the amount
      * @param amount amount
-     * @return
+     * @return result validation
+     * @throws IllegalArgumentException
      */
-    boolean checkAmount(double amount);
+    boolean checkAmount(double amount)throws IllegalArgumentException;
 
     /**
      * Checks availability of funds in the account
-     * @param debet
+     * @param debet account balance
      * @param credit
-     * @return
+     * @return result validation
      * @throws InsufficientFundsException
      */
     boolean checkBalance(double debet, double credit) throws InsufficientFundsException;
